924e8d6dc1d5876844302dd49c260f37ea9de7fc
Cartlong7 <crlong7@gmail.com>
2023-04-19 08:17:17 -0600

    top-k-frequent-elements added

---
+++ src/challenges/arrays-and-hashing/top-k-frequent-elements.ts
@@ -0,0 +1,19 @@
+/* 
+347. Top K Frequent Elements
+
+https://leetcode.com/problems/top-k-frequent-elements/
+
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+Example 1: Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Example 2: nums = [1], k = 1
+Output: [1]
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
