b5eafbe517ce3ad53169eb6fe59ea099dc473111
Cartlong7 <crlong7@gmail.com>
2023-04-19 08:14:55 -0600

    TS solution for groupAnagrams, modified tsconfig to include es2017 syntax

--- src/challenges/arrays-and-hashing/contains-duplicate.ts
+++ src/challenges/arrays-and-hashing/contains-duplicate.ts
@@ -8,7 +8,7 @@ Given an integer array nums, return true if any value appears at least twice in 
 Example 1: Input: nums = [1,2,3,1]
 Output: true 
 
-Example 1: Input: nums = [1,2,3,4]
+Example 2: Input: nums = [1,2,3,4]
 Output: false 
 
 Example 3: Input: nums = [1,1,1,3,3,4,3,2,4,2]
---
+++ src/solutions/arrays-and-hashing-solutions/group-anagrams-solution.ts
@@ -0,0 +1,36 @@
+/* 
+49. Group Anagrams
+
+https://leetcode.com/problems/group-anagrams/
+
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1: Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Example 2: Input: strs = [""]
+Output: [[""]]
+
+Example 3: Input: strs = ["a"]
+Output: [["a"]]
+*/
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+const groupAnagrams = (strs: string[]): string[][] => {
+    const cache: {[key: string]: string[]} = {};
+    for (let str of strs) {
+        const letters = str.split('').sort().join('');
+        if (cache[letters]) {
+            cache[letters].push(str);
+        } else {
+            cache[letters] = [str];
+        }
+    }
+    return Object.values(cache);
+};
