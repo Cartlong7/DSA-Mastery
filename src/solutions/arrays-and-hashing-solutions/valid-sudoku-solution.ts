b005e6fec6e558f7f71bcb327f288b1f64e51f11
Cartlong7 <crlong7@gmail.com>
2023-04-25 13:27:49 -0600

    valid-sudoku-TS

---
+++ src/solutions/arrays-and-hashing-solutions/valid-sudoku-solution.ts
@@ -0,0 +1,72 @@
+/*
+36. Valid Sudoku
+
+https://leetcode.com/problems/valid-sudoku/
+
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+Example 1 : Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+Example 2: Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+*/
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+
+const isValidSudoku = function(board: string[][]): boolean {
+    const rows: Record<number, Set<string>> = {};
+    const cols: Record<number, Set<string>> = {};
+    const squares: Record<string, Set<string>> = {};
+
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            const num = board[r][c];
+
+            if (num === '.') continue;
+
+            const grid = `${Math.floor(r / 3)}${Math.floor(c / 3)}`;
+
+            if (!cols[c]) cols[c] = new Set();
+            if (!rows[r]) rows[r] = new Set();
+            if (!squares[grid]) squares[grid] = new Set();
+            
+            if (rows[r].has(num) || cols[c].has(num) || squares[grid].has(num)) return false;
+            
+            cols[c].add(num);
+            rows[r].add(num);
+            squares[grid].add(num);
+          }
+    }
+    return true;
+};
