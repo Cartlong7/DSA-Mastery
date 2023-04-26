65cb0da53a61314f534ecf55ae23b7d8342d8881
Cartlong7 <crlong7@gmail.com>
2023-04-26 13:53:29 -0600

    longest-consecutive-sequence solution

---
+++ src/TS Solutions/arrays-and-hashing-solutions/longest-consecutive-sequence.ts
@@ -0,0 +1,38 @@
+/* 
+128. Longest Consecutive Sequence
+
+https://leetcode.com/problems/longest-consecutive-sequence/
+
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+Example 1: Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+Example 2: Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+const longestConsecutive = (nums: number[]): number => {
+    const set = new Set(nums);
+    let longest = 0;
+
+    for (const num of nums) {
+        // check if num is the start of a sequence
+        if (!set.has(num - 1)) {
+            let length = 0;
+            while (set.has(num + length)) {
+                length += 1;
+            }
+            longest = Math.max(length, longest);
+        }
+    }
+    return longest;
+};
