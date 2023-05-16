44dd61466da09a90b794b7c263df221aa02bc44d
Cartlong7 <crlong7@gmail.com>
2023-05-15 19:51:28 -0600

    remove element solution

--- src/More algos/Solutions/remove-element-solution.ts
+++ src/More algos/Solutions/remove-element-solution.ts
@@ -29,3 +29,14 @@ It does not matter what you leave beyond the returned k (hence they are undersco
  * @param {number} val
  * @return {number}
  */
+
+const removeElement = (nums: Array<number>, val: number): number => {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] !== val) {
+          nums[count] = nums[i]
+          count += 1;
+      }
+  }
+  return count;
+};
