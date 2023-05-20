3a445b29040a05b62341752ce7d593d84541ab1f
Cartlong7 <crlong7@gmail.com>
2023-05-20 11:13:49 -0600

    add binary problem

---
+++ src/More algos/Challenges/Easy/add-binary.ts
@@ -0,0 +1,21 @@
+/* 
+67. Add Binary
+
+https://leetcode.com/problems/add-binary/
+
+Given two binary strings a and b, return their sum as a binary string.
+
+Example 1: 
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2: 
+Input: a = "1010", b = "1011"
+Output: "10101"
+*/
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
