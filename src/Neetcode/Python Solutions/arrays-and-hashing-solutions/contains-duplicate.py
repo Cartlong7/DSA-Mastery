aeb2c61f415c869ca3eb1b99c0a2b7e133fa7f59
Cartlong7 <crlong7@gmail.com>
2023-06-27 17:09:13 -0600

    two sum py solution and added linting with black

--- src/Neetcode/Python Solutions/arrays-and-hashing-solutions/contains-duplicate.py
+++ src/Neetcode/Python Solutions/arrays-and-hashing-solutions/contains-duplicate.py
@@ -1,26 +1,27 @@
-#217. Contains Duplicate
+# 217. Contains Duplicate
 
-#https://leetcode.com/problems/contains-duplicate/
+# https://leetcode.com/problems/contains-duplicate/
 
-#Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
-#Example 1: Input: nums = [1,2,3,1]
-#Output: true 
+# Example 1: Input: nums = [1,2,3,1]
+# Output: true
 
-#Example 2: Input: nums = [1,2,3,4]
-#Output: false 
+# Example 2: Input: nums = [1,2,3,4]
+# Output: false
 
-#Example 3: Input: nums = [1,1,1,3,3,4,3,2,4,2]
-#Output: true 
+# Example 3: Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
 
 from typing import List
 
-class Solution:
-  def containsDuplicate(self, nums: List[int]) -> bool:
-    hashset = set()
 
-    for n in nums:
-      if n in hashset:
-        return True
-      hashset.add(n)
-    return False
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
--- src/Neetcode/Python Solutions/arrays-and-hashing-solutions/valid-anagram.py
+++ src/Neetcode/Python Solutions/arrays-and-hashing-solutions/valid-anagram.py
@@ -14,19 +14,20 @@
 
 
 # NOTE: you can solve this in python quite easily with sorted() but this solution using a hashmap is:
-  # 1. more optimal in time complexity because most sorting algorithms are at best 0(n log n)
-  # 2. interviewers will most likely want to see a solution that doesn't rely on builtins 
+# 1. more optimal in time complexity because most sorting algorithms are at best 0(n log n)
+# 2. interviewers will most likely want to see a solution that doesn't rely on builtins
+
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-      if len(s) != len(t):
-        return False
-      countS, countT = {}, {}
+        if len(s) != len(t):
+            return False
+        countS, countT = {}, {}
 
-      for i in range(len(s)):
-        countS[s[i]] = 1 + countS.get(s[i], 0)
-        countT[t[i]] = 1 + countT.get(t[i], 0)
-      for c in countS:
-        if countS[c] != countT.get(c, 0):
-          return False
-      return True 
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        return True
