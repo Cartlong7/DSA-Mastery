eb988c73cd78091d1f93733a9cb406a972398eb1
Cartlong7 <crlong7@gmail.com>
2023-07-10 12:52:07 -0600

    min depth of binary tree problem added

---
+++ src/More algos/Challenges/Easy/minimium-depth-of-binary-tree.ts
@@ -0,0 +1,31 @@
+/* 
+111. Minimum Depth of Binary Tree
+
+https://leetcode.com/problems/minimum-depth-of-binary-tree/
+
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example 1: 
+Input: root = [3,9,20,null,null,15,7]
+Output: 2
+
+Example 2: 
+Input: root = [2,null,3,null,4,null,5,null,6]
+Output: 5
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
