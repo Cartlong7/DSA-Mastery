4122f2eedba54ae58f8d4bf3af6274d091068826
Cartlong7 <crlong7@gmail.com>
2023-07-12 11:56:24 -0600

    added py solution folder for general DSA, py solution for is same tree

---
+++ src/DSA/Python Solutions/is-same-tree.py
@@ -0,0 +1,40 @@
+# 100. Same Tree
+
+# https://leetcode.com/problems/same-tree
+
+# Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+# Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+# Example 1:
+# Input: p = [1,2,3], q = [1,2,3]
+# Output: true
+
+# Example 2:
+# Input: p = [1,2], q = [1,null,2]
+# Output: false
+
+# Example 3:
+# Input: p = [1,2,1], q = [1,1,2]
+# Output: false
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+from typing import Optional
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
