c7c14dc74edd10e769fde02dbe2b3ef0964f6d82
Cartlong7 <crlong7@gmail.com>
2023-07-29 10:03:11 -0600

    min stack problem added

---
+++ src/Neetcode/Challenges/stack/min-stack.ts
@@ -0,0 +1,44 @@
+/* 
+155. Min Stack
+
+https://leetcode.com/problems/min-stack/
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+ - MinStack() initializes the stack object.
+ - void push(int val) pushes the element val onto the stack.
+ - void pop() removes the element on the top of the stack.
+ - int top() gets the top element of the stack.
+ - int getMin() retrieves the minimum element in the stack.
+
+You must implement a solution with O(1) time complexity for each function.
+
+Example 1: 
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+*/
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
