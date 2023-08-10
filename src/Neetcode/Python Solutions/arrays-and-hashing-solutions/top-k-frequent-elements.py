b6d8236041d3c68af42dd276343c9f8f3a16c5c9
Cartlong7 <crlong7@gmail.com>
2023-08-10 17:20:30 -0600

    top k frequent elements solution in py

---
+++ src/Neetcode/Python Solutions/arrays-and-hashing-solutions/top-k-frequent-elements.py
@@ -0,0 +1,33 @@
+# 347. Top K Frequent Elements
+
+# https://leetcode.com/problems/top-k-frequent-elements/description/
+
+# Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+# Example 1:
+# Input: nums = [1,1,1,2,2,3], k = 2
+# Output: [1,2]
+
+# Example 2:
+# Input: nums = [1], k = 1
+# Output: [1]
+
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        frequency = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            frequency[c].append(n)
+
+        result = []
+        for i in range(len(frequency) - 1, 0, -1):
+            for n in frequency[i]:
+                result.append(n)
+                if len(result) == k:
+                    return result
