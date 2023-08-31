a8da0049f1f99f0f7c30e2e1188f68528f76bf26
Cartlong7 <crlong7@gmail.com>
2023-08-30 19:01:08 -0600

    binary search 0(log n) solution

--- src/Neetcode/Python Solutions/binary-search-solutions/binary-search.py
+++ src/Neetcode/Python Solutions/binary-search-solutions/binary-search.py
@@ -14,3 +14,20 @@
 # Input: nums = [-1,0,3,5,9,12], target = 2
 # Output: -1
 # Explanation: 2 does not exist in nums so return -1
+
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + ((r - l) // 2)
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
