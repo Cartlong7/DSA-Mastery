1a60a5c6d686503c4342cedf934533c4656fb54a
Cartlong7 <crlong7@gmail.com>
2023-09-08 11:18:13 -0600

    product of array except self python solution

--- src/Neetcode/Python Solutions/arrays-and-hashing-solutions/product-of-array-except-self.py
+++ src/Neetcode/Python Solutions/arrays-and-hashing-solutions/product-of-array-except-self.py
@@ -15,3 +15,22 @@
 # Example 2:
 # Input: nums = [-1,1,0,-3,3]
 # Output: [0,0,9,0,0]
+
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+
+        return res
