921f0aefe39278a327dc6a9b7bb68846c1301e2c
Cartlong7 <crlong7@gmail.com>
2023-09-19 15:28:01 -0600

    max depth of binary tree problem

---
+++ src/DSA/Python Solutions/maximum-depth-of-binary-tree.py
@@ -0,0 +1,15 @@
+# 104. Maximum Depth of Binary Tree
+
+# https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+
+# Given the root of a binary tree, return its maximum depth.
+
+# A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+# Example 1:
+# Input: root = [3,9,20,null,null,15,7]
+# Output: 3
+
+# Example 2:
+# Input: root = [1,null,2]
+# Output: 2
