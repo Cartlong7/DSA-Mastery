f156a3a58741b736dc247e33206cf5ca957a63ce
Cartlong7 <crlong7@gmail.com>
2023-09-21 17:36:02 -0600

    added HackerRank problem Plus Minus

---
+++ src/HackerRank/plus-minus.py
@@ -0,0 +1,36 @@
+# Plus Minus (easy)
+
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-plus-minus/problem
+
+# Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal.
+
+# Function Description
+
+# plusMinus has the following parameter(s):
+
+# int arr[n]: an array of integers
+
+# Print the ratios of positive, negative and zero values in the array. Each value should be printed on a separate line with 6 digits after the decimal. The function should not return a value.
+
+# Input Format
+# The first line contains an integer, n, the size of the array.
+# The second line contains n space-separated integers that describe arr[n].
+
+# Output Format
+# Print the following 3 lines, each to 6 decimals:
+
+# -proportion of positive values
+# -proportion of negative values
+# -proportion of zeros
+
+# Sample Input
+# arr = [-4, 3, -9, 0, 4, 1]
+
+# Sample Output
+# 0.500000
+# 0.333333
+# 0.166667
+
+# Explanation
+# There are 3 positive numbers, 2 negative numbers, and 1 zero in the array.
+# The proportions of occurrence are positive: 3/6 = 0.500000, negative: 3/6 = 0.500000 and zeros: 1/6 = 0.166667
