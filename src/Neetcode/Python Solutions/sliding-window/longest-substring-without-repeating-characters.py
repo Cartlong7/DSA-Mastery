ae155084231b5a2576af5d0b328858ac83573ef2
Cartlong7 <crlong7@gmail.com>
2023-09-22 17:54:26 -0600

    longest substring without repeating character python solution added

--- src/Neetcode/Python Solutions/sliding-window/longest-substring-without-repeating-characters.py
+++ src/Neetcode/Python Solutions/sliding-window/longest-substring-without-repeating-characters.py
@@ -19,3 +19,20 @@
 # Output: 3
 # Explanation: The answer is "wke", with the length of 3.
 # Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+# Sliding window solution
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        left = 0
+        result = 0
+
+        for right in range(len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left += 1
+            charSet.add(s[right])
+            result = max(result, right - left + 1)
+
+        return result
