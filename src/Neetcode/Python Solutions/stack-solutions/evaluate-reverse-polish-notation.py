b6d6ce6f7d878c127606c23e13ae1fe6f65d8cf7
Cartlong7 <crlong7@gmail.com>
2023-09-25 14:46:33 -0600

    Evaluate reverse polish notation solution added

---
+++ src/Neetcode/Python Solutions/stack-solutions/evaluate-reverse-polish-notation.py
@@ -0,0 +1,59 @@
+# 150. Evaluate Reverse Polish Notation
+
+# https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+# You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+# Evaluate the expression. Return an integer that represents the value of the expression.
+
+# Note that:
+# The valid operators are '+', '-', '*', and '/'.
+# Each operand may be an integer or another expression.
+# The division between two integers always truncates toward zero.
+# There will not be any division by zero.
+# The input represents a valid arithmetic expression in a reverse polish notation.
+# The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+
+# Example 1:
+# Input: tokens = ["2","1","+","3","*"]
+# Output: 9
+# Explanation: ((2 + 1) * 3) = 9
+
+# Example 2:
+# Input: tokens = ["4","13","5","/","+"]
+# Output: 6
+# Explanation: (4 + (13 / 5)) = 6
+
+# Example 3:
+# Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+# Output: 22
+# Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+# = ((10 * (6 / (12 * -11))) + 17) + 5
+# = ((10 * (6 / -132)) + 17) + 5
+# = ((10 * 0) + 17) + 5
+# = (0 + 17) + 5
+# = 17 + 5
+# = 22
+
+from typing import List
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        result = 0
+        for char in tokens:
+            if char == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif char == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif char == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif char == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            else:
+                stack.append(int(char))
+        return stack[0]
