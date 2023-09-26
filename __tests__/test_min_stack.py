ab9b0643dc0625eb21c4762b1774ca3e621bc08b
Cartlong7 <crlong7@gmail.com>
2023-09-26 15:12:34 -0600

    added min stack tests

---
+++ __tests__/test_min_stack.py
@@ -0,0 +1,70 @@
+import unittest
+
+
+# MinStack class definition
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+# MinStack Tests
+class TestMinStack(unittest.TestCase):
+    def setUp(self):
+        self.minStack = MinStack()
+
+    def test_push_and_top(self):
+        self.minStack.push(5)
+        self.assertEqual(self.minStack.top(), 5)
+        self.minStack.push(2)
+        self.assertEqual(self.minStack.top(), 2)
+        self.minStack.push(7)
+        self.assertEqual(self.minStack.top(), 7)
+
+    def test_pop(self):
+        self.minStack.push(5)
+        self.minStack.push(2)
+        self.minStack.push(7)
+        self.minStack.pop()
+        self.assertEqual(self.minStack.top(), 2)
+        self.minStack.pop()
+        self.assertEqual(self.minStack.top(), 5)
+
+    def test_getMin(self):
+        self.minStack.push(5)
+        self.assertEqual(self.minStack.getMin(), 5)
+        self.minStack.push(2)
+        self.assertEqual(self.minStack.getMin(), 2)
+        self.minStack.push(7)
+        self.assertEqual(self.minStack.getMin(), 2)
+        self.minStack.push(1)
+        self.assertEqual(self.minStack.getMin(), 1)
+        self.minStack.pop()
+        self.assertEqual(self.minStack.getMin(), 2)
+
+    def test_empty_stack(self):
+        with self.assertRaises(IndexError):
+            self.minStack.top()
+        with self.assertRaises(IndexError):
+            self.minStack.pop()
+        with self.assertRaises(IndexError):
+            self.minStack.getMin()
+
+
+if __name__ == "__main__":
+    unittest.main()
