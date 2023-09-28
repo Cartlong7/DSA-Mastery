c82942f08aece874f6347cac574d102d8ac51792
Cartlong7 <crlong7@gmail.com>
2023-09-28 16:35:31 -0600

    generate parentheses problem added

---
+++ src/Neetcode/Python Solutions/stack-solutions/generate-parentheses.py
@@ -0,0 +1,13 @@
+# 22. Generate Parentheses
+
+# https://leetcode.com/problems/generate-parentheses/
+
+# Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+# Example 1:
+# Input: n = 3
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+# Example 2:
+# Input: n = 1
+# Output: ["()"]
