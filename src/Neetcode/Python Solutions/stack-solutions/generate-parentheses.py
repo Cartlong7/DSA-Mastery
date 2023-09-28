98c94841e78428751464bdcc2ad1198f90fb6ae1
Cartlong7 <crlong7@gmail.com>
2023-09-28 16:36:09 -0600

    generate parentheses python solution

--- src/Neetcode/Python Solutions/stack-solutions/generate-parentheses.py
+++ src/Neetcode/Python Solutions/stack-solutions/generate-parentheses.py
@@ -11,3 +11,27 @@
 # Example 2:
 # Input: n = 1
 # Output: ["()"]
+
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        result = []
+
+        def backtrack(openCount, closeCount):
+            if openCount == closeCount == n:
+                result.append("".join(stack))
+                return
+            if openCount < n:
+                stack.append("(")
+                backtrack(openCount + 1, closeCount)
+                stack.pop()
+            if closeCount < openCount:
+                stack.append(")")
+                backtrack(openCount, closeCount + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+        return result
