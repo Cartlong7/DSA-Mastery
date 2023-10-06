208aab6db130d4f00c553c95653303a15f58d365
Cartlong7 <crlong7@gmail.com>
2023-10-06 16:20:49 -0600

    search a 2d matrix python solution added

--- src/Neetcode/Python Solutions/binary-search-solutions/search-a-2d-matrix.py
+++ src/Neetcode/Python Solutions/binary-search-solutions/search-a-2d-matrix.py
@@ -22,3 +22,36 @@
 # 23 30 34 60
 # Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
 # Output: false
+
+from typing import List
+
+
+# First binary search solution
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows, cols = len(matrix), len(matrix[0])
+        top, bottom = 0, rows - 1
+
+        while top <= bottom:
+            row = (top + bottom) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bottom = row - 1
+            else:
+                break
+        if not top <= bottom:
+            return False
+
+        row = (top + bottom) // 2
+        l, r = 0, cols - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
+        return False
