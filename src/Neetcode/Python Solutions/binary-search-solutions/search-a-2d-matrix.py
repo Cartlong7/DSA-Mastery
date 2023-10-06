bd95b4d0d79035787cf8c4e3b8aafc5e407947f1
Cartlong7 <crlong7@gmail.com>
2023-10-06 16:24:55 -0600

    search a 2d matrix python secondary solution added

--- src/Neetcode/Python Solutions/binary-search-solutions/search-a-2d-matrix.py
+++ src/Neetcode/Python Solutions/binary-search-solutions/search-a-2d-matrix.py
@@ -55,3 +55,23 @@ class Solution:
             else:
                 return True
         return False
+
+
+# Solution two
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows, cols = len(matrix), len(matrix[0])
+        left, right = 0, rows * cols - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            midRow, midCol = divmod(mid, cols)
+
+            if matrix[midRow][midCol] == target:
+                return True
+            elif matrix[midRow][midCol] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
