e045ecd99fcdc22ae491b5c930d87f72195cf88f
Cartlong7 <crlong7@gmail.com>
2023-10-17 11:50:37 -0600

    reverse linked list python solution added

---
+++ src/DSA/Python Solutions/reverse-linked-list.py
@@ -0,0 +1,41 @@
+# 206. Reverse Linked List
+
+# https://leetcode.com/problems/reverse-linked-list/
+
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+# Example 1:
+# Input: head = [1,2,3,4,5]
+# Output: [5,4,3,2,1]
+
+# Example 2:
+# Input: head = [1,2]
+# Output: [2,1]
+
+# Example 3:
+# Input: head = []
+# Output: []
+
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+        prev = None
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        return prev
